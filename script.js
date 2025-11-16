a1e5212fc0a1a8c7f265b6b667c506b929a6a4e6
dimazz <boydima8@gmail.com>
2025-11-16 01:11:39 +0100

    initial commit - clear HTML version

---
+++ script.js
@@ -0,0 +1,11 @@
+// Простое мобильное меню
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".nav-toggle");
+  const nav = document.querySelector(".main-nav");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("open");
+    });
+  }
+});
